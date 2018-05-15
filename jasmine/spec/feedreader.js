986cdad847b3ad17783273692b14748b5113c0e7
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-05-15 17:31:25 +0100

    test: Add a test to ensure feed has at least one entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,12 +78,17 @@ $(function() {
     /* A new test suite named "Initial Entries" */
     describe('Initial Entries', () => {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        beforeEach(done => loadFeed(0, done));
+
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('loads at least one entry', () => {
+            expect(document.querySelectorAll('.entry-link').length > 0).toBe(true);
+        });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
